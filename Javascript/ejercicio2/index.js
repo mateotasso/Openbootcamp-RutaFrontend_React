501cbacd193578dc056728e88ea7ae2b4f837d48
mateo <mateotasso@gmail.com>
2022-11-24 17:55:20 -0300

    add ejercicio

---
+++ Javascript/ejercicio2/index.js
@@ -0,0 +1,24 @@
+/* Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url) */
+
+const list = [
+    "Mateo",
+    25,
+    true,
+    new Date("july 14 1996"),
+    libroFavorito = {
+        titulo: "La noche del oráculo",
+        autor: "Paul Auster",
+        fecha: new Date("november 1 1996"),
+        url: "https://es.wikipedia.org/wiki/La_noche_del_or%C3%A1culo"
+    }
+]
