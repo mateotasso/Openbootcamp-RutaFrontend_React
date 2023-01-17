0b504d66b3bcb34188d20a54d4ae2b18ec48b316
mateo <mateotasso@gmail.com>
2023-01-17 20:13:29 -0300

    add ejercicio 7.1

---
+++ Javascript/ejercicio7/conjuntos.js
@@ -0,0 +1,10 @@
+const familia = ["Homero", "Bart", "Lisa", "Maggie", "Marge"]
+
+// - Un nuevo Set con los nombres de tu familia
+const familia_set = new Set(familia)
+
+// - Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+familia_set.add("Homero")
+
+// - Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;) */
+familia_set.add("Javascript")
